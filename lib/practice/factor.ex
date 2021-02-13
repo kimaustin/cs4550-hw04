aed3e0a9240933c9a48b652b23c40b8d1f812d7a
Austin Kim <kimaustin@Austins-MacBook-Pro.local>
2021-02-13 03:55:10 -0500

    small changes

--- lib/practice/factor.ex
+++ lib/practice/factor.ex
@@ -10,13 +10,13 @@ defmodule Practice.Factor do
 
     def factor(x, factors, next) when rem(x, 2) == 0 do
         # factor(x / 2, factors ++ [2], next)
-        factors ++ [2] ++ factor(floor(x/2))
+        factors ++ [2] ++ factor(round(x/2))
     end
 
     def factor(x, factors, next) do
         if next <= :math.sqrt(x) do 
             if rem(x, next) == 0 do
-                factor(floor(x/next), factors ++ [next], next)
+                factor(round(x/next), factors ++ [next], next)
             else
                 factor(x, factors, next + 2)
             end
